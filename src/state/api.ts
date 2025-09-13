4acd6c1e18afc5d1f32884d9408603e5245bb04b
sudosuanjal <anjalehackchey101@gmail.com>
2025-09-13 14:54:27 +0530

    feat:getAuthUser api

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,12 +1,59 @@
+import { createNewUserInDatabase } from "@/lib/utils";
+import { Manager, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+
+      if (idToken) {
+        headers.set("Authorization", `Bearer ${idToken}`);
+      }
+      return headers;
+    },
   }),
   reducerPath: "api",
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      queryFn: async (_, _queryApi, _extraOptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const { idToken } = session.tokens ?? {};
+          const user = await getCurrentUser();
+          const userRole = idToken?.payload["custom:role"] as string;
+
+          const endpoint =
+            userRole == "manager"
+              ? `/managers/${user.userId}`
+              : `/tenants/${user.userId}`;
+
+          let response = await fetchWithBQ(endpoint);
+          if (response.error && response.error.status === 404) {
+            response = await createNewUserInDatabase(
+              user,
+              idToken,
+              userRole,
+              fetchWithBQ
+            );
+          }
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: response.data as Tenant | Manager,
+              userRole,
+            },
+          };
+        } catch (error: any) {
+          return { error: error.messages || "could not fetch user data" };
+        }
+      },
+    }),
+  }),
 });
 
-export const {} = api;
+export const { useGetAuthUserQuery } = api;
